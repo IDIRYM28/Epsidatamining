525ca5b0bd1a174a9be694fec130621877ce8cc8
IDIRYM28 <idiryahiamessaoud@gmail.com>
2025-01-14 09:56:02 +0100

    comit

--- main.py
+++ main.py
@@ -12,28 +12,26 @@ def get_dataset():
     file_reader = pd.read_csv(file,sep='\t',encoding='utf-8')
     file_reader = file_reader[['Product Price','Product Reviews Count','Product Available Inventory']]
     print('zzzez',file_reader)
+    #calcul des moyenne
     moyenne_prix = file_reader['Product Price'].mean()
     quantity = file_reader['Product Reviews Count'].mean()
     inventory = file_reader['Product Available Inventory'].mean()
+    # remplacer les valeurs null par les moyenne
     file_reader.fillna({'Product Price': moyenne_prix, 'Product Reviews Count': quantity,'Product Available Inventory':inventory}, inplace=True)
+    #normalisation des données
     dataframe = pd.DataFrame(file_reader)
     scaller = MinMaxScaler()
     data_normalized = scaller.fit_transform(dataframe)
-    print(data_normalized)
+    #application de Kmeans
     kmeans = KMeans(n_clusters=3, random_state=42)
-    kmeans.fit(data_normalized)
-    file_reader['cluster'] = kmeans.labels_
-
+    file_reader ['cluster'] = kmeans.fit_predict(data_normalized)
     label_dict = {0: 'faible', 1: 'moyen', 2: 'eleve'}
     file_reader['segment'] = file_reader['cluster'].map({0: 'faible', 1: 'moyen', 2: 'élevé'})
-    print('dddd',file_reader)
     clusters = file_reader.groupby('cluster').mean().reset_index()
     clusters.columns = ['cluster', 'Average Price', 'Average Reviews', 'Average Inventory']
     clusters['Segment Label'] = ['Faible', 'Moyen', 'Élevé']
-
     print('group',clusters)
-
-
+    #visualisation des clusters
     plt.scatter(file_reader['Product Price'], file_reader['Product Reviews Count'], c=file_reader['cluster'], cmap='rainbow')
     plt.xlabel('Product Price')
     plt.ylabel('Product Reviews Count')
